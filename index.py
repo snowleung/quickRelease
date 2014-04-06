a4ec4772b0057478d8cc990ac8519cdf7ef28840
snowleung <snowleung@gmail.com>
2014-04-06 22:25:04 +0800

    init

---
+++ index.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+import web
+
+urls = (
+    '/', 'index'
+)
+
+render = web.template.render('templates/')
+
+class index:
+    def GET(self):
+        return render.index()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
