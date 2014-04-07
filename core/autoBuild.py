d7a1a1d4562166e72377d13e25b1ec77587822c6
snowleung <snowleung@gmail.com>
2014-04-07 22:11:13 +0800

    auto config script

---
+++ core/autoBuild.py
@@ -0,0 +1,48 @@
+#coding:utf-8
+import unittest
+import os
+import random
+from appconf import App_iOS_Config
+
+class autoBuild():
+    def __init__(self):
+        self.builds = []
+    def generateFilesConfig(self, files):
+        for f in files:
+            app = App_iOS_Config()
+            ipa_file = os.path.split(f)[1]
+            ipa_file_name = os.pat.splitext(ipa_file)[0]
+            app.ipa_file = ipa_file
+            app.ipa_bundle_id = str(random.random()*10)[0]
+            app.appxmlurl = ipa_file_name
+            app.title = ipa_file_name
+            self.builds.append(app)
+        return self.builds
+
+class autoBuildtest(unittest.TestCase):
+    def testBuildList(self):
+        abuild = autoBuild()
+        self.assertEqual([], abuild.builds)
+
+    def testReadfile(self):
+        abuild = autoBuild()
+        files = ['/Users/snow/Desktop/jinziqi.ipa']
+        abuild.generateFilesConfig(files)
+        self.assertTrue(len(abuild.builds) == len(files))
+
+    def testFileConfigClass(self):
+        abuild = autoBuild()
+        files = ['/Users/snow/Desktop/jinziqi.ipa']
+        abuild.generateFilesConfig(files)
+        jinziqi_ios_config = abuild.builds[0]
+        self.assertTrue(isinstance(jinziqi_ios_config, App_iOS_Config))
+
+    def testFileConfigTitle(self):
+        abuild = autoBuild()
+        files = ['/Users/snow/Desktop/jinziqi.ipa']
+        abuild.generateFilesConfig(files)
+        jinziqi_ios_config = abuild.builds[0]
+        self.assertTrue(jinziqi_ios_config.title == 'jinziqi')
+
+if __name__ == '__main__':
+    unittest.main()
