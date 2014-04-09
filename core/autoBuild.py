fd3107f80ef0eb25bdfb760ac7320093e9a8ba56
snowleung <snowleung@gmail.com>
2014-04-09 23:09:32 +0800

    1. add ignore file pattern: *.*c 2. split test case to a file, name for your_fileTest.py

--- core/autoBuild.py
+++ core/autoBuild.py
@@ -1,5 +1,4 @@
 #coding:utf-8
-import unittest
 import os
 import random
 from appconf import App_iOS_Config
@@ -21,30 +20,3 @@ class autoBuild():
             self.builds.append(app)
         return len(self.builds)
 
-class autoBuildtest(unittest.TestCase):
-    def testBuildList(self):
-        abuild = autoBuild()
-        self.assertEqual([], abuild.builds)
-
-    def testReadfile(self):
-        abuild = autoBuild()
-        files = ['/Users/snow/Desktop/jinziqi.ipa']
-        abuild.generateFilesConfig(files)
-        self.assertTrue(len(abuild.builds) == len(files))
-
-    def testFileConfigClass(self):
-        abuild = autoBuild()
-        files = ['/Users/snow/Desktop/jinziqi.ipa']
-        abuild.generateFilesConfig(files)
-        jinziqi_ios_config = abuild.builds[0]
-        self.assertTrue(isinstance(jinziqi_ios_config, App_iOS_Config))
-
-    def testFileConfigTitle(self):
-        abuild = autoBuild()
-        files = ['/Users/snow/Desktop/jinziqi.ipa']
-        abuild.generateFilesConfig(files)
-        jinziqi_ios_config = abuild.builds[0]
-        self.assertTrue(jinziqi_ios_config.title == 'jinziqi')
-
-if __name__ == '__main__':
-    unittest.main()
