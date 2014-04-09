00697dc37e49af5594a353a2bd1a8c22f7496058
snowleung <snowleung@gmail.com>
2014-04-09 23:06:13 +0800

    new class init method

--- core/autoBuild.py
+++ core/autoBuild.py
@@ -5,19 +5,21 @@ import random
 from appconf import App_iOS_Config
 
 class autoBuild():
-    def __init__(self):
+    def __init__(self, files = None):
         self.builds = []
+        if files is not None:
+            self.generateFilesConfig(files)
     def generateFilesConfig(self, files):
         for f in files:
             app = App_iOS_Config()
             ipa_file = os.path.split(f)[1]
-            ipa_file_name = os.pat.splitext(ipa_file)[0]
+            ipa_file_name = os.path.splitext(ipa_file)[0]
             app.ipa_file = ipa_file
             app.ipa_bundle_id = str(random.random()*10)[0]
             app.appxmlurl = ipa_file_name
             app.title = ipa_file_name
             self.builds.append(app)
-        return self.builds
+        return len(self.builds)
 
 class autoBuildtest(unittest.TestCase):
     def testBuildList(self):
