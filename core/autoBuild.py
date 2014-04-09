3f49f807ffc0656a1bbe08b4727b299506cc97cc
snowleung <snowleung@gmail.com>
2014-04-10 01:11:06 +0800

    test pass

--- core/autoBuild.py
+++ core/autoBuild.py
@@ -10,13 +10,7 @@ class autoBuild():
             self.generateFilesConfig(files)
     def generateFilesConfig(self, files):
         for f in files:
-            app = App_iOS_Config()
-            ipa_file = os.path.split(f)[1]
-            ipa_file_name = os.path.splitext(ipa_file)[0]
-            app.ipa_file = ipa_file
-            app.ipa_bundle_id = str(random.random()*10)[0]
-            app.appxmlurl = ipa_file_name
-            app.title = ipa_file_name
+            app = App_iOS_Config(f, str(random.random()*10)[0])
             self.builds.append(app)
         return len(self.builds)
 
